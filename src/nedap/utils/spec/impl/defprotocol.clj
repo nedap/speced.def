0ac8c8dc023c7d5b99f89b5a6a068b65ad63bacf
vemv <vemv@users.noreply.github.com>
2019-03-20 18:51:25 +0100

    defprotocol: support for multiple arities
    Fixes #16

--- src/nedap/utils/spec/impl/defprotocol.clj
+++ src/nedap/utils/spec/impl/defprotocol.clj
@@ -6,12 +6,14 @@
    [nedap.utils.spec.impl.parsing :refer [extract-specs-from-metadata]]
    [nedap.utils.spec.impl.type-hinting :refer :all]))
 
+(spec/def ::method-name symbol?)
+(spec/def ::docstring string?)
 (spec/def ::args (spec/coll-of symbol? :kind vector :min-count 1))
 
 (spec/def ::method (spec/and list?
-                             (spec/cat :name symbol?
-                                       :args ::args
-                                       :docstring string?)))
+                             (spec/cat :name ::method-name
+                                       :args (spec/+ ::args)
+                                       :docstring ::docstring)))
 
 (defn emit-method [[method-name args docstring :as method]]
   {:pre [(check! ::method method)]}
@@ -42,25 +44,53 @@
                       tag?       (vary-meta assoc :tag (list 'quote tag))
                       (not tag?) (vary-meta dissoc :tag))
         args-with-proper-tag-hints (strip-extraneous-type-hints args)]
-    {:declare `(declare ~impl)
-     :impl    `(defn ~method-name ~docstring ~args-with-proper-tag-hints ~prepost (~impl ~@args))
-     :method  `(~impl ~args-with-proper-tag-hints ~docstring)}))
+    {:method-name          method-name
+     :protocol-method-name impl
+     :docstring            docstring
+     :declare              `(declare ~impl)
+     :impl-tail            (list args-with-proper-tag-hints prepost (apply list impl args))
+     :proto-tail           args-with-proper-tag-hints}))
 
-(defmacro defprotocol [name docstring & methods]
-  {:pre [(check! symbol? name
-                 string? docstring
-                 ;; (`methods` are already checked in emit-method)
-                 )]}
+(defn extract-signatures [method]
+  {:pre [(check! ::method method)]}
+  (let [name (first method)
+        docstring (last method)
+        argvs (remove #{name docstring} method)]
+    (->> argvs
+         (map (fn [argv]
+                (list name argv docstring))))))
+
+(defn append-to-list [base x]
+  (apply concat (list base (list x))))
+
+(defn consolidate-group
+  "Builds the info for a single protocol method, out of a 'group', namely N signatures of the same method."
+  [group]
+  (let [{:keys [method-name docstring protocol-method-name]} (first group)
+        reduced (->> group
+                     (reduce (fn [acc {:keys [method-name docstring impl-tail declare proto-tail]}]
+                               (-> acc
+                                   (update :fn append-to-list impl-tail)
+                                   (update :proto-decl append-to-list proto-tail)
+                                   (update :declares conj declare)))
+                             {:declares        #{}
+                              :fn              (list 'defn method-name docstring)
+                              :proto-decl      (list protocol-method-name)
+                              :proto-docstring docstring}))]
+    (-> reduced
+        (assoc :impls [(:fn reduced)])
+        (assoc :methods [(:proto-decl reduced)])
+        (update-in [:methods 0] append-to-list (:proto-docstring reduced))
+        (dissoc :fn :proto-decl))))
+
+(defn impl [name docstring methods]
   (let [{:keys [impls methods declares] :as x} (->> methods
+                                                    (mapcat extract-signatures)
                                                     (map emit-method)
-                                                    (reduce (fn [acc {:keys [impl method declare]}]
-                                                              (-> acc
-                                                                  (update :impls conj impl)
-                                                                  (update :declares conj declare)
-                                                                  (update :methods conj method)))
-                                                            {:impls    []
-                                                             :declares []
-                                                             :methods  []}))]
+                                                    (group-by :method-name)
+                                                    (vals)
+                                                    (map consolidate-group)
+                                                    (apply merge-with into))]
 
     `(do
        ~@declares
@@ -71,3 +101,10 @@
        ~@impls
        ;; matches the clojure.core behavior:
        ~(list 'quote name))))
+
+(defmacro defprotocol [name docstring & methods]
+  {:pre [(check! symbol? name
+                 string? docstring
+                 ;; (`methods` are already checked in emit-method)
+                 )]}
+  (impl name docstring methods))
