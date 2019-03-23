ec35c459366002b147383b11d5d3685efe6ba19b
vemv <vemv@users.noreply.github.com>
2019-03-23 02:48:17 +0100

    Expand `pos-integer?` docstring

--- src/nedap/utils/spec/predicates.clj
+++ src/nedap/utils/spec/predicates.clj
@@ -5,7 +5,7 @@
 (speced/defn ^Boolean pos-integer?
   "Is `x` positive (as per `clojure.core/pos?`) and integer (as per `clojure.core/integer?`)?
 
-  This function is recommended over `clojure.core/pos?` and `clojure.pos/pos-int?` for maximum abstraction."
+  This function is recommended over `clojure.core/pos?` and `clojure.pos/pos-int?` for maximum abstraction over specific types."
   [x]
   (and (integer? x)
        (pos? x)))
