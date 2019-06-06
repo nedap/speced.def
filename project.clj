df73836d230ce91b4aa8447c0dbff175a024da5f
vemv <vemv@users.noreply.github.com>
2019-06-06 12:38:06 +0200

    v0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.nedap.staffing-solutions/utils.spec "0.7.0"
+(defproject com.nedap.staffing-solutions/utils.spec "0.8.0"
   :description "clojure.spec utilities"
 
   :url "https://github.com/nedap/utils.spec"
