ada3360cdced36d674d68308bb9da90ccd71e3d8
vemv <vemv@users.noreply.github.com>
2019-10-10 15:47:46 +0200

    Upgrade ci.release-workflow dependency

--- project.clj
+++ project.clj
@@ -88,4 +88,4 @@
              :ci                 {:pedantic?    :abort
                                   :jvm-opts     ["-Dclojure.main.report=stderr"]
                                   :global-vars  {*assert* true} ;; `ci.release-workflow` relies on runtime assertions
-                                  :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.3.0-alpha3"]]}})
+                                  :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.5.0"]]}})
