572a2b9108b7301ae0acb42532f2af1270ce3922
vemv <vemv@users.noreply.github.com>
2019-10-10 12:00:40 +0200

    Expand `integration.nedap.speced.def.reflection`
    Fixes nedap/speced.def#91

--- test/integration/nedap/speced/def/reflection.clj
+++ test/integration/nedap/speced/def/reflection.clj
@@ -31,3 +31,51 @@
   (speced/letfn [(foo [^IFn f]
                    (some-> f .invoke))]
     (foo stop)))
+
+(speced/defn class-inference-sample1 [^string? s]
+  (-> s (.codePointBefore 42)))
+
+(speced/defn class-inference-sample2 [^::speced/nilable ^string? s]
+  (-> s (.codePointBefore 42)))
+
+(speced/defn class-inference-sample3 [{::keys [^string? s]}]
+  (-> s (.codePointBefore 42)))
+
+(speced/defn class-inference-sample4 [{::keys [^::speced/nilable ^string? s]}]
+  (-> s (.codePointBefore 42)))
+
+(defn class-inference-sample5 [x]
+  (speced/let [^string? s x]
+    (-> s (.codePointBefore 42))))
+
+(defn class-inference-sample6 [x]
+  (speced/let [^::speced/nilable ^string? s x]
+    (-> s (.codePointBefore 42))))
+
+(defn class-inference-sample7 [x]
+  (speced/let [{:keys [^string? s]} x]
+    (-> s (.codePointBefore 42))))
+
+(defn class-inference-sample8 [x]
+  (speced/let [{:keys [^::speced/nilable ^string? s]} x]
+    (-> s (.codePointBefore 42))))
+
+(defn class-inference-sample9 [x]
+  (speced/letfn [(foo [^string? s]
+                   (-> s (.codePointBefore 42)))]
+    (foo x)))
+
+(defn class-inference-sample10 [x]
+  (speced/letfn [(foo [^::speced/nilable ^string? s]
+                   (-> s (.codePointBefore 42)))]
+    (foo x)))
+
+(defn class-inference-sample11 [x]
+  (speced/letfn [(foo [{::keys [^string? s]}]
+                   (-> s (.codePointBefore 42)))]
+    (foo x)))
+
+(defn class-inference-sample12 [x]
+  (speced/letfn [(foo [{::keys [^::speced/nilable ^string? s]}]
+                   (-> s (.codePointBefore 42)))]
+    (foo x)))
