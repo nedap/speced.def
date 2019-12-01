7cec45ddb1db39f31c9a4a6ff5acabf696451f2b
vemv <vemv@users.noreply.github.com>
2019-12-01 10:23:29 +0100

    Reapply our Lein template

--- project.clj
+++ project.clj
@@ -30,6 +30,8 @@
 
   :target-path "target/%s"
 
+  :test-paths ["src" "test"]
+
   :monkeypatch-clojure-test false
 
   :plugins [[lein-cljsbuild "1.1.7"]
@@ -59,21 +61,18 @@
                                                  [com.stuartsierra/component "0.4.0"]
                                                  [com.taoensso/timbre "4.10.0"]
                                                  [criterium "0.4.4"]
-                                                 [formatting-stack "1.0.0-alpha3"
-                                                  :exclusions [rewrite-clj]]
+                                                 [formatting-stack "1.0.0"]
                                                  [lambdaisland/deep-diff "0.0-29"]
-                                                 [medley "1.1.0"]
-                                                 [org.clojure/core.async "0.4.490"]
+                                                 [medley "1.2.0"]
+                                                 [org.clojure/core.async "0.5.527"]
                                                  [org.clojure/math.combinatorics "0.1.1"]
                                                  [org.clojure/test.check "0.10.0-alpha3"]
-                                                 [org.clojure/tools.namespace "0.3.0-alpha4"]
-                                                 [org.clojure/tools.reader "1.1.1" #_"transitive"]
-                                                 [rewrite-clj "0.6.1" #_"transitive"]]
-                                  :plugins      [[lein-cloverage "1.0.13"]]
-                                  :source-paths ["dev" "test"]
+                                                 [org.clojure/tools.namespace "0.3.0-alpha4"]]
+                                  :plugins      [[lein-cloverage "1.1.1"]]
+                                  :source-paths ["dev"]
                                   :repl-options {:init-ns dev}}
 
-             :provided           {:dependencies [[org.clojure/clojurescript "1.10.520"
+             :provided           {:dependencies [[org.clojure/clojurescript "1.10.597"
                                                   :exclusions [com.cognitect/transit-clj
                                                                com.google.code.findbugs/jsr305
                                                                com.google.errorprone/error_prone_annotations]]
@@ -81,11 +80,12 @@
                                                  [com.google.errorprone/error_prone_annotations "2.1.3" #_"transitive"]
                                                  [com.google.code.findbugs/jsr305 "3.0.2" #_"transitive"]]}
 
-             :test               {:dependencies [[com.nedap.staffing-solutions/utils.test "1.5.0"]]}
+             :test               {:dependencies [[com.nedap.staffing-solutions/utils.test "1.6.1"]]
+                                  :jvm-opts     ["-Dclojure.core.async.go-checking=true"]}
 
              :warn-on-reflection {:global-vars {*warn-on-reflection* true}}
 
              :ci                 {:pedantic?    :abort
                                   :jvm-opts     ["-Dclojure.main.report=stderr"]
                                   :global-vars  {*assert* true} ;; `ci.release-workflow` relies on runtime assertions
-                                  :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.5.0"]]}})
+                                  :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.6.0"]]}})
