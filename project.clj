5dae8dcdf6685031cc94cccb0f7d4ccc830a46b9
vemv <vemv@users.noreply.github.com>
2020-01-17 02:12:52 +0100

    v2.0.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;; Please don't bump the library version by hand - use ci.release-workflow instead.
-(defproject com.nedap.staffing-solutions/speced.def "2.0.0-alpha1"
+(defproject com.nedap.staffing-solutions/speced.def "2.0.0"
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[com.nedap.staffing-solutions/utils.spec "1.2.0-alpha1"]
                  [com.nedap.staffing-solutions/utils.test "1.6.2"]
